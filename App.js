9b88dc996a2169e215aa27009dfd5b09ea50a554
Abraham E. Tavarez <abetavarez@icloud.com>
2022-01-31 22:49:26 -0500

    done

--- App.js
+++ App.js
@@ -1,1 +1,13 @@
 console.log(`Running script...`);
+
+const display = document.querySelector('.display');
+const increaseBtn = document.getElementById('increase')
+const decreaseBtn = document.querySelector('#decrease')
+
+const increase = () => display.textContent = Number(display.textContent)+1;
+
+const decrease = () => display.textContent = Number(display.textContent) - 1;
+
+
+increaseBtn.addEventListener('click', increase);
+decreaseBtn.addEventListener('click', decrease);
